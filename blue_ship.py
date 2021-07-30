00f13c4aee9cdb03e912b28f978904986bacac44
Jathur <jathursikan@gmail.com>
2021-07-30 14:02:34 -0400

    Creating the blue ship class and file

---
+++ blue_ship.py
@@ -0,0 +1,7 @@
+import pygame
+
+class BlueShip:
+    """This is the class for the blue ship in alien wars"""
+    def __init__(self, game):
+        """Initialize the blue ship and its attributes"""
+        self.screen = game.screen
