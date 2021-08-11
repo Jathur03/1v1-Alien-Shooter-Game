b0ef7c64f9bf98b9d6060e255097b2fafa1f5349
Jathur <jathursikan@gmail.com>
2021-08-11 13:59:30 -0400

    Movement

--- ships.py
+++ ships.py
@@ -16,35 +16,9 @@ class Ships:
         self.red_ship_image = pygame.image.load('img/red.png')
         self.red_ship_rect = self.red_ship_image.get_rect()
 
-        # Making each ship to start in the left bottom part of the screen
+        # Making each ship to start in the bottom part of the screen
         self.blue_ship_rect.bottomleft = self.screen_rect.bottomleft
         self.red_ship_rect.bottomright = self.screen_rect.bottomright
-
-        # Store a deicmal value for the ship's horizontal position
-        self.x = float(self.blue_ship_rect.x)
-
-        # Store a deciaml value for the ship's verticle position
-        self.y = float(self.blue_ship_rect.y)
-
-        # Moving flags
-        self.moving_left = False
-        self.moving_right = False
-        self.moving_up = False
-        self.moving_down = False
-
-    def update_ship(self):
-        """Change the ships position"""
-        if self.moving_left:
-            self.x -= self.settings.ship_speed
-
-        if self.moving_right:
-            self.x += self.settings.ship_speed
-
-        if self.moving_up:
-            self.y -= self.settings.ship_speed
-
-        if self.moving_down:
-            self.x += self.settings.ship_speed
 
     def blitme(self):
         """Draw the blue ship at its current location"""
