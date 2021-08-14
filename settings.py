d4c7cc24a26e88040c6cf241c414a26a6feec624
Sajan Paventhan <70298158+Sajan734@users.noreply.github.com>
2021-08-14 18:06:21 -0400

    eeeee
    both ship movement work

--- settings.py
+++ settings.py
@@ -4,6 +4,9 @@ class Settings:
     """This is the class where all the settings for the game will be"""
     def __init__(self):
         """A method to initialize the game settings"""
+
+        self.ship_speed = 1.5
+
         # Screen size
         self.screen_width = 1200
         self.screen_height = 750
@@ -13,3 +16,4 @@ class Settings:
 
         # Saving an image for the app icon
         self.app_icon = pygame.image.load('img/app-icon.png')
+
--- ships.py
+++ ships.py
@@ -33,7 +33,7 @@ class BShips(Sprite):
             self.y -= self.settings.ship_speed
 
         # Update rect object from self.y.
-        self.y = self.rect.y
+        self.rect.y = self.y
     
     # def center_ship(self):
     #     self.rect.bottomleft = self.screen_rect.bottomleft
@@ -81,3 +81,44 @@ class BShips(Sprite):
 #     def blitme(self):
 #         """Draw the blue ship at its current location"""
 #         self.screen.blit(self.red_ship_image, self.red_ship_rect)
+
+class RShips(Sprite):
+    """This is the class for the ships"""
+    def __init__(self, game):
+        super().__init__()
+        """Initialize the ships and its attributes"""
+        self.screen = game.screen
+        self.screen_rect = game.screen.get_rect()
+        self.settings = game.settings
+
+
+        # Saving the ships' image to a variable and getting its rect
+        self.image = pygame.image.load('img/red.png')
+        self.rect = self.image.get_rect()
+
+        # Making each ship to start in the bottom part of the screen
+        self.rect.bottomright = self.screen_rect.bottomright
+
+        self.y = float(self.rect.y)
+
+        self.red_moving_up = False
+        self.red_moving_down = False
+    
+    def update(self):
+        """Update the ship's position based on movement flags."""
+        # Update the ship's y value, not the rect
+        if self.red_moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        if self.red_moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+
+        # Update rect object from self.y.
+        self.rect.y = self.y
+    
+    # def center_ship(self):
+    #     self.rect.bottomleft = self.screen_rect.bottomleft
+    #     self.
+
+    def blitme(self):
+        """Draw the blue ship at its current location"""
+        self.screen.blit(self.image, self.rect)    
